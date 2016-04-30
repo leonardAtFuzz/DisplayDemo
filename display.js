7001726a096417e96cf14d21e14a8bf58be8b481
leonardAtFuzz <leonard@fuzzproductions.com>
2016-04-30 14:08:31 -0400

    Update display.js

--- display.js
+++ display.js
@@ -45,9 +45,10 @@ function onCreateView(rootContainer){
 
 var counter = 0;
 function onRender(view, data){
-    console.log(data.total_users);
     counter++;
     myChart.data.labels.push(""+counter);
     myChart.data.datasets[0].data.push(data.total_users);
     myChart.update();
+        console.log(""+ counter + " : " + data.total_users);
+
 }
